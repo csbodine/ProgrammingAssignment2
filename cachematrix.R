574f462adcb47189fb4b743414a7f1328c9fbb79
Clint Bodine <clintonbodine@fico.com>
2016-03-06 22:59:36 -0700

    Write code to mirrror Assignment example.

--- cachematrix.R
+++ cachematrix.R
@@ -6,20 +6,30 @@
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL;
     set <- function(y) {
-      x <<- y
-      tryCatch(m <<- solve(y), 
-               warning = function(w) {warning(paste("Matrix is not invertible"))},
-               error = function(e) {warning(paste("Matrix is not invertible"))}
-               );
+      x <<- y;
+      m <<- NULL;
+      return(x);
     }
-    get <- function() {x}
-    getInverse <- function() {m}
-    list(set = set, get = get, getInverse = getInverse)
+    get <- function() {x};
+    setInverse <- function(y) { m <<- y; };
+    getInverse <- function() { m };
+    list(set = set, 
+         get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse();
+        if(!is.null(i)) {
+          message("getting cached inverse");
+          return(i);
+        }
+        mtrx <- x$get();
+        i <- solve(mtrx);
+        x$setInverse(i);
+        return(i);
 }
